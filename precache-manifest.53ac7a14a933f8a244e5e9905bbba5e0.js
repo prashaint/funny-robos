f3c8e985400416f5e5d4f96103bd0ccd7b135625
Prashaint Mishra <prashaint_mishra@hotmail.com>
2020-06-21 13:37:54 +0530

    Updates

---
+++ precache-manifest.53ac7a14a933f8a244e5e9905bbba5e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10fa3bf92b4d0db008ec44cd34702b34",
+    "url": "/funny-robos/index.html"
+  },
+  {
+    "revision": "d481b821c391abcb5ade",
+    "url": "/funny-robos/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "7c73e64aed9356f649e3",
+    "url": "/funny-robos/static/css/main.171b34c9.chunk.css"
+  },
+  {
+    "revision": "d481b821c391abcb5ade",
+    "url": "/funny-robos/static/js/2.383c4612.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/funny-robos/static/js/2.383c4612.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c73e64aed9356f649e3",
+    "url": "/funny-robos/static/js/main.82eceeaa.chunk.js"
+  },
+  {
+    "revision": "a1c2d7fd9778a2e359cc",
+    "url": "/funny-robos/static/js/runtime-main.9d066b07.js"
+  },
+  {
+    "revision": "ea3d606596c5d79b422aad1516d4710e",
+    "url": "/funny-robos/static/media/Lobster-Regular.ea3d6065.woff"
+  }
+]);
